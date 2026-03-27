81d07be4fb4aeb40756b56e754e2346203e97e1a
Jagadeeswar reddy Vankala <jagadeshreddyvankala123@gmail.com>
2026-03-27 20:02:55 +0530

    function

---
+++ DataStructures/Functions/function1.py
@@ -0,0 +1,28 @@
+def greet():
+    return "Hello world!"
+
+print(greet())
+
+
+def greet(name):
+    return "Hello "+name
+
+
+print(greet("Jagadeesh"))
+
+def add(a,b):
+    return a+b
+
+print(add(10,9))
+
+def multiply(a,b,c):
+    return a*b*c
+
+print(multiply(5,6,7))
+
+def greet(name="surendra"):
+    return name+" King"
+
+print(greet("Rajesh "))
+
+print(greet())
