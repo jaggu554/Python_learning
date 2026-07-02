a785105ec7e6510e77b7abb2bde9dedfde42ddd2
Jagadeeswar reddy Vankala <jagadeshreddyvankala123@gmail.com>
2026-07-02 22:06:39 +0530

    restarting the python

---
+++ Variables/variable1.py
@@ -0,0 +1,22 @@
+name ="JagadeeshReddy"
+age=25
+experience_2026=90000
+city="Anantapur"
+
+print(name)
+print(age)
+print(experience_2026)
+print(city)
+
+print(type(name))
+print(type(age))
+print(type(experience_2026))
+print(type(city))
+
+one=two=three=10
+a=20
+b=30
+a,b=b,a
+
+print(a)
+print(b)
